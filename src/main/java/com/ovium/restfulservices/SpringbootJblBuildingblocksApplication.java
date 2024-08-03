4462dbd2cfa5eb58c5f02721a6e490e9f780df9f
Lovely Job <JobLovely@gmail.com>
2024-08-03 14:26:48 -0500

    First Commit

---
+++ src/main/java/com/ovium/restfulservices/SpringbootJblBuildingblocksApplication.java
@@ -0,0 +1,13 @@
+package com.ovium.restfulservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJblBuildingblocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJblBuildingblocksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ovium/restfulservices/SpringbootJblBuildingblocksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ovium.restfulservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJblBuildingblocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
