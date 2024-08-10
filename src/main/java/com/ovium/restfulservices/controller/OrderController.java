801b4cc185f1798b6ebeb407b2ec8bc48de2d5ed
joblovely <JobLovely@gmail.com>
2024-08-10 18:51:56 -0500

    06-SpringBoot-JPA-One2Many - First Commit.

---
+++ src/main/java/com/ovium/restfulservices/controller/OrderController.java
@@ -0,0 +1,59 @@
+package com.ovium.restfulservices.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ovium.restfulservices.entities.Order;
+import com.ovium.restfulservices.entities.User;
+import com.ovium.restfulservices.exceptions.UserNotFoundException;
+import com.ovium.restfulservices.repositories.OrderRepository;
+import com.ovium.restfulservices.repositories.UserRepository;
+
+@RestController
+@RequestMapping(value = "/users")
+public class OrderController {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Autowired
+	private OrderRepository orderRepository;
+
+	// get All Orders for a user
+
+	@GetMapping("/{userid}/orders")
+	public List<Order> getAllOrders(@PathVariable Long userid) throws UserNotFoundException {
+
+		Optional<User> userOptional = userRepository.findById(userid);
+		if (!userOptional.isPresent()) {
+			throw new UserNotFoundException("User Not Found");
+		}
+
+		return userOptional.get().getOrders();
+	}
+
+	// Create Order
+
+	@PostMapping("{userid}/orders")
+	public Order createOrder(@PathVariable Long userid, @RequestBody Order order) throws UserNotFoundException {
+		Optional<User> userOptional = userRepository.findById(userid);
+
+		if (!userOptional.isPresent()) {
+			throw new UserNotFoundException("User Not Found");
+		}
+
+		User user = userOptional.get();
+		order.setUser(user);
+		return orderRepository.save(order);
+
+	}
+
+}
--- src/main/java/com/ovium/restfulservices/entities/User.java
+++ src/main/java/com/ovium/restfulservices/entities/User.java
@@ -1,9 +1,12 @@
 package com.ovium.restfulservices.entities;
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
@@ -37,6 +40,9 @@ public class User {
 
 	@Column(name = "SSN", length = 50, nullable = false, unique = true)
 	private String ssn;
+
+	@OneToMany(mappedBy = "user")
+	private List<Order> orders;
 
 	// No Argument Constructor
 	public User() {
@@ -108,6 +114,14 @@ public class User {
 
 	public void setSsn(String ssn) {
 		this.ssn = ssn;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
+	}
+
+	public void setOrders(List<Order> orders) {
+		this.orders = orders;
 	}
 
 	// To String
